13e3c8a47241da11cc130d4677d6f4bae9b3b243
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-01-03 14:25:27 +0200

    try

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+import React, { useRef, useState, useEffect } from 'react';
+
+function App() {
+  const videoRef = useRef(null);
+  const [devices, setDevices] = useState([]);
+  const [deviceId, setDeviceId] = useState(null);
+
+  useEffect(() => {
+    async function getCameras() {
+      // Get a list of available media devices
+      const deviceList = await navigator.mediaDevices.enumerateDevices();
+
+      // Filter the list to only include videoinput devices (cameras)
+      const cameras = deviceList.filter(device => device.kind === 'videoinput');
+
+      // Set the state with the list of cameras
+      setDevices(cameras);
+    }
+
+    getCameras();
+  }, []);
+
+  const startVideo = async () => {
+    try {
+      // Get the stream from the selected camera
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          deviceId: deviceId,
+        },
+      });
+      videoRef.current.srcObject = stream;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      {devices.length > 1 && (
+        <>
+          <label htmlFor="camera-select">Camera:</label>
+          <select id="camera-select" onChange={e => setDeviceId(e.target.value)}>
+            {devices.map(device => (
+              <option value={device.deviceId}>{device.label}</option>
+            ))}
+          </select>
+        </>
+      )}
+      <button onClick={startVideo}>Start Video</button>
+      <video ref={videoRef} autoPlay />
+    </div>
+  );
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+
+export default App;
